6738402ac814a46e3a214ce530084041a2620814
MeanMachineMarc <marcus9981@icloud.com>
2023-04-12 18:31:17 +0100

    subHeader, cardText & boldCardText

--- lib/elements.dart
+++ lib/elements.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class Elements {
   static const cardHeader = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.bold,
+    color: Colours.lightGray,
+  );
+
+  static const subHeader = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colours.lightGray,
+  );
+
+  static const cardText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colours.lightGray,
+  );
+
+  static const boldCardText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colours.lightGray,
   );
 }
