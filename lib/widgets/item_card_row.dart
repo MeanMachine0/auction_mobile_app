8fb055afa0c5e6df34c78c6875f004f7d8e5beee
MeanMachineMarc <marcus9981@icloud.com>
2023-04-18 21:04:38 +0100

    new

---
+++ lib/widgets/item_card_row.dart
@@ -0,0 +1,30 @@
+import 'package:auction_mobile_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class ItemCardRow extends StatelessWidget {
+  const ItemCardRow({
+    super.key,
+    required String text,
+    bool isBold = false,
+  })  : _text = text,
+        _isBold = isBold;
+
+  final String _text;
+  final bool _isBold;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Flexible(
+        fit: FlexFit.loose,
+        child: Text(
+          _text,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+          textAlign: TextAlign.start,
+          style: _isBold ? Elements.boldCardText : Elements.cardText,
+        ),
+      ),
+    ]);
+  }
+}
